33987825acbad92288253940fdd646b063139d74
JitenderSinghh <vblearnerofficial@gmail.com>
2016-10-13 01:44:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
 console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
 
-
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+button.onclick = function(){
+    
+    //make a request to counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in correct span
+    counter = counter + 1;
+    var span = document.getElementById('span');
+    span.innerHTML = counter.toString();
 };
