ae734de2e4f43e1b499cec07bc8d78ef92f0dff5
JitenderSinghh <vblearnerofficial@gmail.com>
2016-11-18 22:40:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,22 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name',function(req,res){
-    //get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
